e4e55917512ce2fda153ccb695138f94d00ff328
SuperLinguini <milind.lingineni@gmail.com>
2017-07-05 19:54:18 -0400

    Added KMeans and EM as features.

--- Project 2/adult.py
+++ Project 2/adult.py
@@ -50,14 +50,14 @@ df.head(10)
 
 df = pd.get_dummies(df, columns=['Workclass', 'Marital Status', 'Occupation', 'Relationship', 'Race'])
 
-X_train, X_test, y_train, y_test = train_test_split(
+X_train_unscaled, X_test_unscaled, y_train, y_test = train_test_split(
     df[list(set(df.columns) - set(['Target']))],
     df['Target'], train_size=0.70)
-# scaler = preprocessing.StandardScaler()
-# X_train = pd.DataFrame(scaler.fit_transform(X_train.astype('float32')), columns=X_train.columns)
-# X_test = scaler.transform(X_test.astype('float32'))
+scaler = preprocessing.StandardScaler()
+X_train = pd.DataFrame(scaler.fit_transform(X_train_unscaled.astype('float32')), columns=X_train_unscaled.columns)
+X_test = scaler.transform(X_test_unscaled.astype('float32'))
 
-def kmeans(train_data, test_data, output_str, n_splits=5):
+def kmeans(train_data, test_data, output_str, i=0):
     # if type(train_data) is np.ndarray:
     #     train_set = pd.DataFrame(train_data)
     # if type(test_data) is np.ndarray:
@@ -87,7 +87,7 @@ def kmeans(train_data, test_data, output_str, n_splits=5):
         test_score = k_means.score(test_data)
         end_test = time.time() - start_test
 
-        values = [n, test_score / n_splits, end_train/n_splits, end_test/n_splits]
+        values = [n, test_score, end_train, end_test]
         results.loc[len(results)] = values
 
         print(' '.join(str(col) for col in columns))
@@ -96,16 +96,18 @@ def kmeans(train_data, test_data, output_str, n_splits=5):
     # plt.show(results['Num Clusters'], results['Test Score'])
     ax = results.plot(x='Num Clusters', y='Score', title='KMeans ' + output_str)
     fig = ax.get_figure()
-    fig.savefig(os.path.join('Images','adult_kmeans_{}.png'.format(output_str)))
+    fig.savefig(os.path.join('Images','adult_kmeans_{}{}.png'.format(output_str, i)))
 
     results.to_excel('adult_kmeans_{}.xls'.format(output_str))
 
 
-def expectation_maximization(train_data, test_data, output_str, n_splits=5):
+def expectation_maximization(train_data, test_data, output_str, i=0):
     # if type(train_data) is np.ndarray:
     #     train_set = pd.DataFrame(train_data)
-    # if type(test_data) is np.ndarray:
     #     test_set = pd.DataFrame(test_data)
+    #     train_set = train_set.append(test_set)
+    # else:
+    #     train_set = train_data
 
     n_clusters = [x for x in range(2,11)]
 
@@ -116,7 +118,6 @@ def expectation_maximization(train_data, test_data, output_str, n_splits=5):
     for n in n_clusters:
         # end_train = 0
         # end_test = 0
-        # cv_score = 0
         # test_score = 0
 
         # for train_indices, test_indices in kf.split(train_set):
@@ -132,7 +133,7 @@ def expectation_maximization(train_data, test_data, output_str, n_splits=5):
         test_score = gm.score(test_data)
         end_test = time.time() - start_test
 
-        values = [n, test_score / n_splits, end_train / n_splits, end_test / n_splits]
+        values = [n, test_score, end_train, end_test]
         results.loc[len(results)] = values
 
         print(' '.join(str(col) for col in columns))
@@ -140,62 +141,141 @@ def expectation_maximization(train_data, test_data, output_str, n_splits=5):
 
     ax = results.plot(x='Num Clusters', y='Score', title='Expectation Maximization ' + output_str)
     fig = ax.get_figure()
-    fig.savefig(os.path.join('Images','adult_expectation_maximization_{}.png'.format(output_str)))
+    fig.savefig(os.path.join('Images','adult_expectation_maximization_{}{}.png'.format(output_str, i)))
 
     results.to_excel('adult_expectation_maximization_{}.xls'.format(output_str))
 
-def overall():
-    kmeans(X_train, X_test, 'Overall')
-    expectation_maximization(X_train, X_test, 'Overall')
+def overall(i):
+    kmeans(X_train, X_test, 'Overall', i)
+    expectation_maximization(X_train, X_test, 'Overall', i)
+    # neural_network(X_train, y_train, X_test, y_test, 'Overall', i)
 
-def principal_component_analysis():
+def principal_component_analysis(i):
     pca = PCA(n_components=20)
     X_train_pca = pca.fit_transform(X_train)
     X_test_pca = pca.transform(X_test)
-    kmeans(X_train_pca, X_test_pca, 'PCA')
-    expectation_maximization(X_train_pca, X_test_pca, 'PCA')
-    neural_network(X_train_pca, y_train, X_test_pca, y_test)
+    kmeans(X_train_pca, X_test_pca, 'PCA', i)
+    expectation_maximization(X_train_pca, X_test_pca, 'PCA', i)
+    # neural_network(X_train_pca, y_train, X_test_pca, y_test, 'PCA', i)
 
-def independent_component_analysis():
+def independent_component_analysis(i):
     ica = FastICA(n_components=20)
     X_train_ica = ica.fit_transform(X_train)
     X_test_ica = ica.transform(X_test)
-    kmeans(X_train_ica, X_test_ica, 'ICA')
-    expectation_maximization(X_train_ica, X_test_ica, 'ICA')
-    neural_network(X_train_ica, y_train, X_test_ica, y_test)
+    kmeans(X_train_ica, X_test_ica, 'ICA', i)
+    expectation_maximization(X_train_ica, X_test_ica, 'ICA', i)
+    # neural_network(X_train_ica, y_train, X_test_ica, y_test, 'ICA', i)
 
-def randomized_projection():
+def randomized_projection(i):
     rp = GaussianRandomProjection(n_components=20)
     X_train_rp = rp.fit_transform(X_train)
     X_test_rp = rp.transform(X_test)
-    kmeans(X_train_rp, X_test_rp, 'RandomizedProjection')
-    expectation_maximization(X_train_rp, X_test_rp, 'RandomizedProjection')
+    kmeans(X_train_rp, X_test_rp, 'RandomizedProjection', i)
+    expectation_maximization(X_train_rp, X_test_rp, 'RandomizedProjection', i)
+    # neural_network(X_train_rp, y_train, X_test_rp, y_test, 'RandomizedProjection', i)
 
-def latent_dirichlet_allocation():
+def latent_dirichlet_allocation(i):
     lda = LatentDirichletAllocation(n_topics=20, learning_method='batch', n_jobs=-1)
-    X_train_lda = lda.fit_transform(X_train)
-    X_test_lda = lda.transform(X_test)
-    # kmeans(X_train_lda, X_test_lda, 'LDA')
-    # expectation_maximization(X_train_lda, X_test_lda, 'LDA')
-    neural_network(X_train_lda, y_train, X_test_lda, y_test)
+    X_train_lda = lda.fit_transform(X_train_unscaled)
+    X_test_lda = lda.transform(X_test_unscaled)
+    kmeans(X_train_lda, X_test_lda, 'LDA', i)
+    expectation_maximization(X_train_lda, X_test_lda, 'LDA', i)
+    # neural_network(X_train_lda, y_train, X_test_lda, y_test, 'LDA', i)
 
-def neural_network(X_train, y_train, X_test, y_test):
+def neural_network(X_train, y_train, X_test, y_test, output_str, i):
+    # X_train2 = X_train.as_matrix()
+    # train_validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+    #     X_train,
+    #     y_train,
+    #     every_n_steps=100)
+    test_validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+        X_test,
+        y_test,
+        every_n_steps=50)
+
 
     tf.logging.set_verbosity(tf.logging.INFO)
     feature_columns = [tf.contrib.layers.real_valued_column("", dimension=X_train.shape[1])]
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                                 hidden_units=[10, 10],
-                                                n_classes=2,
-                                                model_dir="/tmp/adult_nn3")
-    classifier.fit(x=X_train, y=y_train, steps=1600)
-    accuracy_score = classifier.evaluate(x=X_test, y=y_test, steps=1)["accuracy"]
+                                                # dropout=0.5,
+                                                model_dir="/tmp/adult_nn_{}{}".format(output_str, i),
+                                                config=tf.contrib.learn.RunConfig(save_checkpoints_steps=100))
+    if i % 2 == 0:
+        setattr(classifier, 'dropout', 0.5)
+    classifier.fit(x=X_train, y=y_train, steps=1500, monitors=[test_validation_monitor])
+    accuracy_score = classifier.evaluate(x=X_test, y=y_test, steps=50)["accuracy"]
 
     print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
+def kmeans_as_feature(i=0):
+    n_clusters = [3,4,7]
+
+    for n in n_clusters:
+        k_means = KMeans(n_clusters=n, n_jobs=-1).fit(X_train)
+        # test_score = k_means.score(test_data)
+
+        if type(X_train) is np.ndarray:
+            X_train_feature = pd.DataFrame(X_train.copy())
+        else:
+            X_train_feature = X_train.copy()
+        X_train_feature.index = range(X_train_feature.shape[0])
+        X_train_feature['Clusters'] = pd.Series(k_means.predict(X_train_feature))
+
+        if type(X_test) is np.ndarray:
+            X_test_feature = pd.DataFrame(X_test.copy())
+        else:
+            X_test_feature = X_test.copy()
+        X_test_feature.index = range(X_test_feature.shape[0])
+        X_test_feature['Clusters'] = pd.Series(k_means.predict(X_test_feature))
+
+        neural_network(X_train_feature, y_train, X_test_feature, y_test, 'KMeansFeature{}_'.format(i), n)
+
+def expectation_maximization_as_feature(i=0):
+    n_clusters = [3,4,8]
+
+    for n in n_clusters:
+        gm = GaussianMixture(n_components=n).fit(X_train)
+        # test_score = k_means.score(test_data)
+
+        if type(X_train) is np.ndarray:
+            X_train_feature = pd.DataFrame(X_train.copy())
+        else:
+            X_train_feature = X_train.copy()
+        X_train_feature.index = range(X_train_feature.shape[0])
+        X_train_feature['Clusters'] = pd.Series(gm.predict(X_train_feature))
+
+        if type(X_test) is np.ndarray:
+            X_test_feature = pd.DataFrame(X_test.copy())
+        else:
+            X_test_feature = X_test.copy()
+        X_test_feature.index = range(X_test_feature.shape[0])
+        X_test_feature['Clusters'] = pd.Series(gm.predict(X_test_feature))
+
+        neural_network(X_train_feature, y_train, X_test_feature, y_test, 'EMFeatures{}_'.format(i), n)
+
+def double_data(i):
+    X_train_feature = X_train.append(X_train)
+    y_train_feature = y_train.append(y_train)
+
+    if type(X_test) is np.ndarray:
+        X_test_feature = pd.DataFrame(X_test.copy())
+    else:
+        X_test_feature = X_test.copy()
+
+    neural_network(X_train_feature, y_train_feature, X_test_feature, y_test, 'Double{}_'.format(i), i)
+
+
 if __name__ == '__main__':
-    # overall()
-    #
-    # principal_component_analysis()
-    # independent_component_analysis()
-    # randomized_projection()
-    latent_dirichlet_allocation()
+
+    for i in range(4):
+        overall(i)
+
+        principal_component_analysis(i)
+        independent_component_analysis(i)
+        randomized_projection(i)
+        latent_dirichlet_allocation(i)
+    # for i in range(4):
+    #     kmeans_as_feature(i)
+    #     expectation_maximization_as_feature(i)
+    #     double_data(i)
